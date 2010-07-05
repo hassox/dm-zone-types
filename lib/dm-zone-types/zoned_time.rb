5d99f7a39ed323dca1a255cd6bf474e5c3c165cf
Daniel Neighman <has.sox@gmail.com>
2010-07-05 11:29:43 +1000

    Some updates to toughen it up to deal with the awesomeness that is oracle

--- lib/dm-zone-types/zoned_time.rb
+++ lib/dm-zone-types/zoned_time.rb
@@ -6,13 +6,17 @@ module DataMapper
     class ZonedTime < Time
       include DataMapper::Zone::Types
 
+      def valid?(value)
+        value.kind_of?(::Time) || value.nil?
+      end
+
       def load(value)
         value.nil? ? nil : value.in_time_zone(::Time.zone)
       end
 
       def dump(value)
         return nil unless value
-        value.in_time_zone(DataMapper::Zone::Types.storage_zone)
+        time_with_zone_to_primitive value.in_time_zone(DataMapper::Zone::Types.storage_zone)
       end
 
       def typecast(value)
@@ -31,6 +35,12 @@ module DataMapper
           nil
         end
       end
+
+      private
+      def time_with_zone_to_primitive(value)
+        value.to_datetime.to_time
+      end
     end
   end
 end
+
